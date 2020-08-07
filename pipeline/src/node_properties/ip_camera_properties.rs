9b0e1edeb00a6007906aadbc78aac31f427a3497
Zeeshan Ali <zeeshan@lumeo.com>
2020-08-07 15:42:21 +0200

    pipeline: Add `IpCameraProperties::udp_port`
    While it's currently not used, it makes sense to have this proprety so
    doesn't hurt to have it already as optional.

--- pipeline/src/node_properties/ip_camera_properties.rs
+++ pipeline/src/node_properties/ip_camera_properties.rs
@@ -25,6 +25,11 @@ pub struct IpCameraRuntime {
     /// Stream ID is used by `lumeod` to add a WebRTC endpoint to webrtcstreamer service.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub stream_id: Option<String>,
+
+    /// UDP port.
+    ///
+    /// Currently unused.
+    pub udp_port: Option<u16>,
 }
 
 impl_camera_props!(IpCameraProperties, IpCameraRuntime);
