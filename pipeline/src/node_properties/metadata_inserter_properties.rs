1606770f46b18a35f4f2676a4b24ff1a55b47503
Zeeshan Ali <zeeshan@lumeo.com>
2020-09-18 17:07:19 +0200

    Add MetadataInserter node properties

---
+++ pipeline/src/node_properties/metadata_inserter_properties.rs
@@ -0,0 +1,11 @@
+use serde::{Deserialize, Serialize};
+use url::Url;
+
+/// Insert metadata frames from an external source.
+///
+/// Reads lines (newline delimited) of TEXT from the given URI and appends them to the frames. The
+/// only supported scheme currently is `lumeo`, which will use TCP as transport.
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct MetadataInserterProperties {
+    pub uri: Url,
+}
