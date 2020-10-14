6ec2ae3161114e875de84930c82d589230203c53
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2020-10-14 23:46:27 +0700

    Update Clip node fields according to the latest spec

--- pipeline/src/node_properties/clip_properties.rs
+++ pipeline/src/node_properties/clip_properties.rs
@@ -2,22 +2,41 @@ use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use url::Url;
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(tag = "location", rename_all = "snake_case")]
+pub enum ClipProperties {
+    Local(LocalClipProperties),
+    LumeoCloud(LumeoCloudClipProperties),
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct ClipProperties {
+pub struct CommonClipProperties {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub min_duration: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_duration: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub max_size: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub retention_duration: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub webhook_url: Option<Url>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub trigger: Option<String>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct LocalClipProperties {
+    #[serde(flatten)]
+    pub common: CommonClipProperties,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub path: Option<PathBuf>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub max_duration_sec: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub max_size_bytes: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
     pub max_edge_files: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub edge_retention_duration: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub cloud_upload: Option<bool>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub cloud_retention_duration: Option<u64>,
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub webhook_url: Option<Url>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct LumeoCloudClipProperties {
+    #[serde(flatten)]
+    pub common: CommonClipProperties,
 }
