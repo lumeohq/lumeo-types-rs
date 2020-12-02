88fa7ae6e37e67e2893a5306d13988a398eb917d
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2020-12-02 17:21:25 +0700

    Add gateway online/offline events

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -2,9 +2,11 @@ use serde::{Deserialize, Serialize};
 
 pub mod camera;
 pub mod deployment;
+pub mod gateway;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Event {
+    Gateway(gateway::Event),
     Deployment(deployment::Event),
     Camera(camera::Event),
 }
