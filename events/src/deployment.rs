22f38746a45785c2d278a3d578686937ddcf65ca
Zeeshan Ali <zeeshan@lumeo.com>
2021-01-20 13:25:07 +0100

    events: Add GstError event
    This will be used to send gstreamer errors from lumeod to the web
    services.

--- events/src/deployment.rs
+++ events/src/deployment.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use zvariant::derive::Type;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Event {
@@ -13,4 +14,21 @@ pub enum DeploymentEventKind {
     Stopped,
     StopFailed,
     ExitedUnexpectedly,
+    GstError(GstError),
+}
+
+#[derive(Serialize, Deserialize, Type, Debug, Clone)]
+pub enum GstErrorDomain {
+    Core = 1,
+    Library = 2,
+    Resource = 3,
+    Stream = 4,
+}
+
+#[derive(Serialize, Deserialize, Type, Debug, Clone)]
+pub struct GstError {
+    pub domain: GstErrorDomain,
+    // FIXME: We probaly could do better than this and use an enum here.
+    pub code: i32,
+    pub description: String,
 }
