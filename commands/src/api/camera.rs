f61c8385eecda096a503ca8a4f9bbb8685351559
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2021-04-27 22:09:33 +0700

    Remove obsolete requests

--- commands/src/api/camera.rs
+++ commands/src/api/camera.rs
@@ -1,59 +1,18 @@
 use serde::{Deserialize, Serialize};
-use thiserror::Error;
 use url::Url;
 use uuid::Uuid;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum Request {
-    /// Discover cameras. Returned data can be incomplete if camera
-    /// requires authorization. For authorized access use `GetWithAuth`.
+    /// Discover cameras
     Discover { request_id: Uuid },
 
     /// Create camera streams
     CreateStreams { camera_id: Uuid },
-
-    /// Get local camera information
-    GetLocal { url: Url },
-
-    /// Get camera information
-    GetWithAuth {
-        url: Url,
-        credentials: Option<Credentials>,
-    },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct DiscoverResponse(pub Vec<Camera>);
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct GetLocalResponse(pub Result<LocalCamera, GetLocalError>);
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct GetWithAuthResponse(pub Result<Camera, GetWithAuthError>);
-
-#[derive(Serialize, Deserialize, Debug, Clone, Error)]
-pub enum GetLocalError {
-    #[error("Camera is offline")]
-    CameraOffline,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone, Error)]
-pub enum GetWithAuthError {
-    #[error("Camera is offline")]
-    CameraOffline,
-
-    #[error("Invalid credentials")]
-    InvalidCredentials,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct Credentials {
-    /// Camera user name
-    pub username: String,
-
-    /// Plaintext password
-    pub password: String,
-}
 
 /// `Camera` type represents different camera types.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
