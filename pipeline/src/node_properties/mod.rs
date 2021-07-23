5a1f58df1e4037475d0b77756b9a6b968edfe119
SIGLOST <rui.silva.dev@gmail.com>
2021-07-23 16:24:36 +0100

    Sort modules and NodeProperties elements alphabetically

--- pipeline/src/node_properties/mod.rs
+++ pipeline/src/node_properties/mod.rs
@@ -2,71 +2,72 @@
 
 use serde::{Deserialize, Serialize};
 
+pub mod clip_properties;
+pub mod combine_properties;
+pub mod encode_properties;
+pub mod function_properties;
+pub mod grid_properties;
+pub mod gst_template_properties;
+pub mod metadata_inserter_properties;
+pub mod model_inference_properties;
+pub mod overlay_properties;
+pub mod snapshot_properties;
+#[macro_use]
+pub mod stream_properties;
+pub mod stream_rtsp_out_properties;
+pub mod stream_web_rtc_out_properties;
+pub mod track_properties;
+pub mod transform_properties;
 pub mod video_source_properties;
+
+pub use clip_properties::{
+    ClipProperties, CommonClipProperties, LocalClipProperties, LumeoCloudClipProperties,
+};
+pub use combine_properties::CombineProperties;
+pub use encode_properties::EncodeProperties;
+pub use function_properties::{FunctionProperties, FunctionRuntime};
+pub use grid_properties::GridProperties;
+pub use gst_template_properties::GstTemplateProperties;
+pub use metadata_inserter_properties::MetadataInserterProperties;
+pub use model_inference_properties::{
+    ClassInferenceProperties, ModelInferenceProperties, ModelInferenceRuntime,
+};
+pub use overlay_properties::OverlayProperties;
+pub use snapshot_properties::{
+    CommonSnapshotProperties, LocalSnapshotProperties, LumeoCloudSnapshotProperties,
+    SnapshotProperties,
+};
+pub use stream_properties::{StreamProperties, StreamRuntime};
+pub use stream_rtsp_out_properties::{StreamRtspOutProperties, StreamRtspOutRuntime};
+pub use stream_web_rtc_out_properties::{StreamWebRtcOutProperties, StreamWebRtcOutRuntime};
+pub use track_properties::{TrackProperties, Tracker, TrackerProfile};
+pub use transform_properties::{FlipDirection, TransformProperties};
 pub use video_source_properties::{
     CameraProperties, CameraRuntime, CommonVideoSourceProperties, InputRtspStreamRuntime,
     InputStreamProperties, InputStreamRuntime, InputWebRtcStreamRuntime, RotateDirection,
     UsbCameraRuntime, VideoSourceProperties,
 };
-#[macro_use]
-pub mod stream_properties;
-pub use stream_properties::{StreamProperties, StreamRuntime};
-pub mod encode_properties;
-pub use encode_properties::EncodeProperties;
-pub mod gst_template_properties;
-pub use gst_template_properties::GstTemplateProperties;
-pub mod stream_rtsp_out_properties;
-pub use stream_rtsp_out_properties::{StreamRtspOutProperties, StreamRtspOutRuntime};
-pub mod stream_web_rtc_out_properties;
-pub use stream_web_rtc_out_properties::{StreamWebRtcOutProperties, StreamWebRtcOutRuntime};
-pub mod transform_properties;
-pub use transform_properties::{FlipDirection, TransformProperties};
-pub mod model_inference_properties;
-pub use model_inference_properties::{
-    ClassInferenceProperties, ModelInferenceProperties, ModelInferenceRuntime,
-};
-pub mod metadata_inserter_properties;
-pub use metadata_inserter_properties::MetadataInserterProperties;
-pub mod overlay_properties;
-pub use overlay_properties::OverlayProperties;
-pub mod clip_properties;
-pub use clip_properties::{
-    ClipProperties, CommonClipProperties, LocalClipProperties, LumeoCloudClipProperties,
-};
-pub mod snapshot_properties;
-pub use snapshot_properties::{
-    CommonSnapshotProperties, LocalSnapshotProperties, LumeoCloudSnapshotProperties,
-    SnapshotProperties,
-};
-pub mod track_properties;
-pub use track_properties::{TrackProperties, Tracker, TrackerProfile};
-pub mod function_properties;
-pub use function_properties::{FunctionProperties, FunctionRuntime};
-pub mod combine_properties;
-pub use combine_properties::CombineProperties;
-pub mod grid_properties;
-pub use grid_properties::GridProperties;
 
 #[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
 #[allow(clippy::large_enum_variant)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum NodeProperties {
-    #[serde(rename = "video")]
-    VideoSource(VideoSourceProperties),
+    Clip(ClipProperties),
+    Combine(CombineProperties),
     Encode(EncodeProperties),
-    Transform(TransformProperties),
-    ModelInference(ModelInferenceProperties),
+    Function(FunctionProperties),
+    Grid(GridProperties),
+    GstTemplate(GstTemplateProperties),
     #[serde(rename = "metadata_add")]
     MetadataInserter(MetadataInserterProperties),
+    ModelInference(ModelInferenceProperties),
     Overlay(OverlayProperties),
-    Clip(ClipProperties),
     Snapshot(SnapshotProperties),
     StreamRtspOut(StreamRtspOutProperties),
     #[serde(rename = "stream_webrtc_out")]
     StreamWebRtcOut(StreamWebRtcOutProperties),
     Track(TrackProperties),
-    Function(FunctionProperties),
-    GstTemplate(GstTemplateProperties),
-    Combine(CombineProperties),
-    Grid(GridProperties),
+    Transform(TransformProperties),
+    #[serde(rename = "video")]
+    VideoSource(VideoSourceProperties),
 }
