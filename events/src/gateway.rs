9e58ae75b6489f8130d2f5f67583953b14ee6158
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2021-09-24 20:51:10 +0700

    Remove `lumeo-events` crate
    As we no longer send events over MQTT.

--- events/src/gateway.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(tag = "status", rename_all = "lowercase")]
-pub enum Event {
-    Online { version: String },
-    Offline,
-}
