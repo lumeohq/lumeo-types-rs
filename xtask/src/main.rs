3ac258c8e7fcea5c92dd1d504d2068391f2fdeb4
Jonas Platte <jonas@lumeo.com>
2021-10-13 19:42:03 +0200

    Update xtask check-pipeline to accept separate definition & configuration

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -9,15 +9,14 @@
     clippy::unseparated_literal_suffix,
     clippy::wildcard_imports
 )]
+#![allow(clippy::suspicious_else_formatting)]
 
-use std::{
-    error::Error,
-    io::{self, Read},
-    process,
-};
+use std::{collections::BTreeMap, fs, process};
 
+use anyhow::Context;
 use clap::Clap;
 use lumeo_pipeline::Pipeline;
+use serde_json::Value as JsonValue;
 
 #[derive(Clap)]
 struct Options {
@@ -28,14 +27,14 @@ struct Options {
 #[derive(Clap)]
 enum SubCommand {
     /// Checks whether a JSON pipeline definition can be deserialized correctly (reads from stdin)
-    CheckPipeline,
+    CheckPipeline { pipeline_file: String, configuration_file: Option<String> },
 }
 
 fn main() {
     let opt = Options::parse();
     match opt.subcmd {
-        SubCommand::CheckPipeline => {
-            if let Err(e) = check_pipeline() {
+        SubCommand::CheckPipeline { pipeline_file, configuration_file } => {
+            if let Err(e) = check_pipeline(&pipeline_file, configuration_file.as_deref()) {
                 eprintln!("{}", e);
                 process::exit(1);
             }
@@ -43,15 +42,50 @@ fn main() {
     }
 }
 
-fn check_pipeline() -> Result<(), Box<dyn Error + 'static>> {
-    let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf)?;
+fn check_pipeline(pipeline_file: &str, configuration_file: Option<&str>) -> anyhow::Result<()> {
+    type Configuration = BTreeMap<String, serde_json::Map<String, JsonValue>>;
 
-    match serde_json::from_str::<Pipeline>(&buf) {
+    let mut pipeline_json = fs::read_to_string(pipeline_file)?;
+
+    if let Some(file) = configuration_file {
+        let configuration_json = fs::read_to_string(file)?;
+        let configuration = match serde_json::from_str::<Configuration>(&configuration_json) {
+            Ok(config) => config,
+            Err(e) => {
+                eprint!("Invalid pipeline configuration: ");
+                return Err(e.into());
+            }
+        };
+
+        update_pipeline_def(&mut pipeline_json, &configuration).context("configuring pipeline")?;
+    }
+
+    match serde_json::from_str::<Pipeline>(&pipeline_json) {
         Ok(_) => Ok(()),
         Err(e) => {
-            eprint!("Deserialization failed: ");
+            eprint!("Invalid pipeline definition: ");
             Err(e.into())
         }
     }
+}
+
+fn update_pipeline_def(
+    pipeline_json: &mut String,
+    configuration: &BTreeMap<String, serde_json::Map<String, JsonValue>>,
+) -> anyhow::Result<()> {
+    let mut pipeline_nodes: Vec<JsonValue> = serde_json::from_str(pipeline_json)?;
+
+    for node in &mut pipeline_nodes {
+        let node_id = node["id"].as_str().context("id is not a string")?.to_owned();
+        let props_obj =
+            node["properties"].as_object_mut().context("properties is not an object")?;
+
+        if let Some(config_props) = configuration.get(&node_id) {
+            props_obj.extend(config_props.clone());
+        }
+    }
+
+    *pipeline_json = serde_json::to_string(&pipeline_nodes)?;
+
+    Ok(())
 }
