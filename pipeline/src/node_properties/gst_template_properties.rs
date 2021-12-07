cbaa6ad500989dfd7e2219c8c527beb6343e2076
Dmitry Samoylov <dmitry.samoylov@quantumsoft.ru>
2021-12-07 23:25:06 +0700

    Remove `pipeline` crate
    It is moved into `api-client` repo.

--- pipeline/src/node_properties/gst_template_properties.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-use serde_json::{Map, Value};
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub struct GstTemplateProperties {
-    pub definition: String,
-    #[serde(default)]
-    pub props: Map<String, Value>,
-}
